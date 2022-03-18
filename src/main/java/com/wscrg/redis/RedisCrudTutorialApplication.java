b62d0c3b1f5650e37ec35c32e5952b54ffe04289
wscrg <wldyd2356@gmail.com>
2022-03-18 19:46:46 +0900

    Initial Commit

---
+++ src/main/java/com/wscrg/redis/RedisCrudTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.wscrg.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCrudTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisCrudTutorialApplication.class, args);
+    }
+
+}
